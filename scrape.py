f02e8acf87d9fe7d53f5efcb3bc0dcbc1165e58b
Ivana2105 <44193334+Ivana2105@users.noreply.github.com>
2018-10-27 01:14:20 +0200

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 17 09:26:11 2018
+Created on Sat Oct 20 17:29:30 2018
 
 @author: Ivana-ETF
 """
@@ -9,28 +9,25 @@ from bs4 import BeautifulSoup
 import argparse
 import requests
 from HTMLTable import * 
+import sys
 
    
 
 def get_table(url, sport, element, iid, index, clas):
     
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    res = requests.get(url,headers=headers)
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)'
+               ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95'
+               ' Safari/537.36'}
+    res = requests.get(url, headers=headers)
     plain_text = res.text 
-    soup = BeautifulSoup(plain_text,"html.parser")
-    table = soup.find_all("table")
+    soup = BeautifulSoup(plain_text, 'html.parser')
+    table = soup.find_all('table')
     
     # Check if there are tables on the page
     if len(table):
         # Check which html element is selected
         if element == "table":
-            if index:
-                if int(index) in range(len(table)):
-                    parse(table[int(index)], sport, element, iid, index, clas)
-                else:
-                    print('There is no table with index {}'\
-                          .format(str(index)))                   
-            else:
+            if iid or clas :
                 attr = 'id' if iid else 'class'
                 val = iid if iid else clas
                 b = 0
@@ -48,8 +45,14 @@ def get_table(url, sport, element, iid, index, clas):
                             parse(table[i], sport, element, iid, index, clas)
 
                 if not b:
-                    print('There is no atribute {} with value {}'\
-                          .format(attr, val))
+                    print('There is no attribute {} with value {}'
+                          .format(attr, val))                 
+            else:
+                if int(index) in range(len(table)):
+                    parse(table[int(index)], sport, element, iid, index, clas)
+                else:
+                    print('There is no table with index {}'
+                          .format(str(index)))  
                     
         else:
             parse(table[0], sport, element, iid, index, clas)
@@ -63,16 +66,11 @@ def get_table(url, sport, element, iid, index, clas):
         if not b:
             print('There is no table and iframe on the page')
             
-            
-def Main():
-    """ 
-    User select required input parameters - test url and sport as 
-    required input parameters and optional parameters - html elements
-    and html attributes 
-     
-    """
+def parse_args(args):
+    
     parser = argparse.ArgumentParser()
-    parser.add_argument("url", help="choose the Staff Directory page URL to be scraped")
+    parser.add_argument("url", help="choose the Staff Directory page URL to "
+                        "be scraped")
     parser.add_argument("sport", help="choose sport to be filtered")
     parser.add_argument("--html_elem", help="choose html element",\
                         choices=['table','tr','td'])
@@ -80,20 +78,25 @@ def Main():
     group.add_argument('--element_id')
     group.add_argument('--element_index')
     group.add_argument('--element_class')
-    args = parser.parse_args()
-    if (args.html_elem and args.element_id is None 
-        and args.element_index is None and args.element_class is None):
-        parser.error("--html_elem requires --element_id or --element_index or --element_class")
     
-    url = args.url
-    sport = args.sport
-    element = args.html_elem
-    iid = args.element_id
-    index = args.element_index
-    clas = args.element_class
-                        
-    get_table(url, sport, element, iid, index, clas)
+    return parser.parse_args(args)            
+            
+def Main():
+
+    parser = parse_args(sys.argv[1:])
+    if (parser.html_elem and parser.element_id is None 
+        and parser.element_index is None and parser.element_class is None):
+        print('--html_elem requires --element_id or --element_index ' 
+              'or --element_class')
+        exit()
+                            
+    get_table(parser.url, parser.sport, parser.html_elem, parser.element_id, 
+              parser.element_index, parser.element_class)
     
 
 if __name__=='__main__':
     Main()
+    
+    
+
+
